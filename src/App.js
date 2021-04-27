ab7caa6a4fb683f33cc823132fbc8cffff958b22
YekaterinaPavlenko <pavluka1605@gmail.com>
2021-04-27 21:31:40 +0300

    меняет класс на ф-цию

--- src/App.js
+++ src/App.js
@@ -1,32 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import ContactForm from './components/ContactsForm/ContactsForm';
 import ContactList from './components/ContactList/ContactList';
 import Filter from './components/Filter/Filter';
-import { connect } from 'react-redux';
-import * as allContactsAction from './redux/allContacts/allContactsAction';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Phonebook</h1>
-        <ContactForm />
-        <h2>Contacts</h2>
-        <Filter />
-        <ContactList />
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <h2>Contacts</h2>
+      <Filter />
+      <ContactList />
+    </div>
+  );
+};
 
-const mapStateToProps = store => {
-  return {
-    contacts: store.contacts.items,
-  };
-};
-const mapDispatchToProps = {
-  getContacts: allContactsAction.getAllContacts,
-  addContact: allContactsAction.addContact,
-};
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
